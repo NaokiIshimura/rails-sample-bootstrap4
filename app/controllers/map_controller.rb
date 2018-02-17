d0a654ef59deeec034c37dd3327ba31c1c47db47
Naoki Ishimura <mirontest1460@gmail.com>
2018-02-17 09:59:31 +0900

    Add-filter

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,47 +1,71 @@
 class MapController < ApplicationController
   def sample
     # 地図
-    @zoom = 15
+    @zoom   = 15
     @center = { lat: '35.681298', lng: '139.766247' }
-  
+    
     # 円
     @radius = 100
-  
+    
     # マーカー
     @location = set_location
-  
-    # 情報ウィンドウ
-    @window = set_window
   end
   
+  def filter
+    # 地図
+    @zoom   = 15
+    @center = { lat: '35.681298', lng: '139.766247' }
+    
+    # 円
+    @radius = 100
+    
+     #フィルタ
+    @filter_list = %w(filter_1 filter_2 filter_3 filter_4)
+    
+    @filter = params[:filter]
+    @filter ||= 'filter_1'
+    
+    # マーカー
+    @location = set_location(@filter)
+  end
+  
+  
   private
-
-  def set_location
-    base_data = [
-        { lat: 35.681298 + 0.005, lng: 139.766247 },
-        { lat: 35.681298 - 0.005, lng: 139.766247 },
-        { lat: 35.681298, lng: 139.766247 + 0.005 },
-        { lat: 35.681298, lng: 139.766247 - 0.005 },
-        { lat: 35.681298 + 0.01, lng: 139.766247 },
-        { lat: 35.681298 - 0.01, lng: 139.766247 },
-        { lat: 35.681298, lng: 139.766247 + 0.01 },
-        { lat: 35.681298, lng: 139.766247 - 0.01 },
-    ]
   
+  def set_location(filter = 'filter_1')
+  
+    base_data = []
+  
+    if filter[-1].to_i.odd? == true
+      base_data = [
+          { lat: 35.681298 + 0.005, lng: 139.766247 },
+          { lat: 35.681298 - 0.005, lng: 139.766247 },
+          { lat: 35.681298, lng: 139.766247 + 0.005 },
+          { lat: 35.681298, lng: 139.766247 - 0.005 },
+      ]
+    else
+      base_data = [
+          { lat: 35.681298 + 0.01, lng: 139.766247 },
+          { lat: 35.681298 - 0.01, lng: 139.766247 },
+          { lat: 35.681298, lng: 139.766247 + 0.01 },
+          { lat: 35.681298, lng: 139.766247 - 0.01 },
+      ]
+    end
+    
     location = []
-  
+    
     base_data.each do |data|
       location.push(
           label: 'in',
-          lat: data[:lat],
-          lng: data[:lng],
-          icon: 'http://mt.google.com/vt/icon/name=icons/spotlight/star_L_8x.png&scale=1'
+          lat:   data[:lat],
+          lng:   data[:lng],
+          icon:  'http://mt.google.com/vt/icon/name=icons/spotlight/star_L_8x.png&scale=1'
       )
     end
-  
+    
     location
   end
-
+  
   def set_window
     base_data = [
         { lat: 35.681298 + 0.0025, lng: 139.766247 },
@@ -49,18 +73,18 @@ class MapController < ApplicationController
         { lat: 35.681298, lng: 139.766247 + 0.0025 },
         { lat: 35.681298, lng: 139.766247 - 0.0025 }
     ]
-  
+    
     window = []
-  
+    
     base_data.each do |data|
       window.push(
           label: 'out',
-          lat: data[:lat],
-          lng: data[:lng]
+          lat:   data[:lat],
+          lng:   data[:lng]
       )
     end
-  
+    
     window
   end
-  
+
 end
