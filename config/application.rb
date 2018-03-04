ff4d985c7b360d81d376730cb3805b01e73ef334
Naoki Ishimura <mirontest1460@gmail.com>
2018-03-04 11:06:52 +0900

    Add japanese langage config.

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,9 @@ module RailsSampleBootstrap4
     # Set time zone to Tokyo
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
-    
+
+    # i18n
+    # config/locales/xx.yml
+    config.i18n.default_locale = :ja
   end
 end
